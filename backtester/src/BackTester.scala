f04655ac40512d828d9f8c2bb06aa8122d664a02
gio <[omitted]>
2020-11-24 12:52:34 +0100

    Updates Table classes

--- backtester/src/BackTester.scala
+++ backtester/src/BackTester.scala
@@ -1,9 +1,16 @@
 package backtester
 
-import scala.concurrent._, duration.Duration.Inf, java.util.concurrent.Executors
 import backtester.Db._
+import slick.jdbc.PostgresProfile.api._
+import java.sql.Timestamp
+import scala.concurrent._, duration.Duration.Inf, java.util.concurrent.Executors
 
 object BackTester extends App {
   println("You called a BackTester, Here I am")
-  Await.result(query(), Inf).foreach(println(_))
+  val future = Db run {
+    TableQuery[BidAskTicksNQ20200918]
+      .filter(_.time > Timestamp.valueOf("2020-09-18 15:29:52.0"))
+      .result
+  }
+  Await.result(future, Inf).foreach(println(_))
 }
--- backtester/src/Db.scala
+++ backtester/src/Db.scala
@@ -1,12 +1,30 @@
 package backtester
 
 import slick.jdbc.PostgresProfile.api._
-import scala.concurrent._, duration.Duration.Inf, java.util.concurrent.Executors
+import scala.concurrent.ExecutionContext.Implicits.global
 import com.typesafe.config.ConfigFactory
-import java.io._
+import java.io.File
 import java.sql.Timestamp
 
-class BidAskTicks(tag: Tag)
+abstract class BidAskTicks(tag: Tag, tableName: String, schemaName: Option[String])
+  extends Table[(Timestamp, Double, Double, Int, Int, Boolean, Boolean, String)](
+    tag, _tableName=tableName, _schemaName=schemaName
+  ) {
+  def time = column[Timestamp]("time")
+  def bid = column[Double]("bid")
+  def ask = column[Double]("ask")
+  def bid_size = column[Int]("bid_size")
+  def ask_size = column[Int]("ask_size")
+  def bid_decrease = column[Boolean]("bid_decrease")
+  def ask_increase = column[Boolean]("ask_increase")
+  def pk = column[String]("pk", O.PrimaryKey)
+  def * = (time, bid, ask, bid_size, ask_size, bid_decrease, ask_increase, pk)
+}
+
+class BidAskTicksNQ20200918(tag: Tag)
+  extends BidAskTicks(tag, "nq20200918_371749745_bid_ask", Some("ticks"))
+
+class OldBidAskTicks(tag: Tag)
   extends Table[(Timestamp, Double, Double, Int, Int, Boolean, Boolean, String)](
     tag, _tableName="test", _schemaName=Some("ticks")
   ) {
@@ -22,15 +40,8 @@ class BidAskTicks(tag: Tag)
 }
   
 object Db {
-  implicit val ec = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(8))
-  
   val fileConfig = ConfigFactory.parseFile(new File("application.conf"))
   val config = ConfigFactory.load(fileConfig)
   
-  val db = Database.forConfig(path="postgres", config=config)
-
-  def query() = {
-    val future = db.run(TableQuery[BidAskTicks].filter(_.time > Timestamp.valueOf("2020-09-18 15:29:52.0")).result)
-    future
-  }
+  val Db = Database.forConfig(path="postgres", config=config)
 } 
