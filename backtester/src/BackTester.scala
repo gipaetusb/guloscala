65606d72732b4fdbd80b32e4e5ca1e4e9f1a804a
gio <[omitted]>
2020-11-24 22:03:34 +0100

    Adds some slick examples

--- backtester/src/BackTester.scala
+++ backtester/src/BackTester.scala
@@ -3,14 +3,40 @@ package backtester
 import backtester.Db._
 import slick.jdbc.PostgresProfile.api._
 import java.sql.Timestamp
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._, duration.Duration.Inf, java.util.concurrent.Executors
+import scala.util.{Success, Failure}
 
 object BackTester extends App {
   println("You called a BackTester, Here I am")
-  val future = Db run {
-    TableQuery[BidAskTicksNQ20200918]
-      .filter(_.time > Timestamp.valueOf("2020-09-18 15:29:52.0"))
-      .result
+
+  val tradeTicks = TableQuery[TradeTicksNQ20200918]
+  val bidaskTicks = TableQuery[BidAskTicksNQ20200918]
+  val bidaskTicksGrp = TableQuery[BidAskTicksGrpNQ20200918]
+  
+  val largeTradeTicks = tradeTicks.filter(tick => tick.size > 100)
+  // calling .result transforms the query into an action, which I can transform into a future using db.run
+  db run largeTradeTicks.result onComplete {
+    case Success(values) => println(values)
+    case Failure(e) => println(e)
   }
-  Await.result(future, Inf).foreach(println(_))
+
+  trait TickTrai
+  case class Tick(time: java.sql.Timestamp, price: Double, size: Int, bid: Double, ask: Double) extends TickTrai
+  
+  val largeTrades = for {
+    (trade, bidAsk) <- largeTradeTicks join bidaskTicksGrp on (_.time === _.time)
+  } yield (trade.time, trade.price, trade.size, bidAsk.bid, bidAsk.ask)
+  val f = db run largeTrades.result
+  f.filter(t => 
+    t match {
+      case Seq(_, price, _, _, _) => price > 0
+      case _ => false
+      //case (time, price, size, bid, ask): Seq(, Double, Int, Double, Double) => price <= bid || price >= ask})
+    }
+  ) 
+
+  //largeTrades.filter(t => t match {case Some(size) => size > 1 case None => false})
+  
 }
+// NOTE: tick => tick.time > Timestamp.valueOf("2020-09-18 15:29:52.0")
--- backtester/src/Db.scala
+++ backtester/src/Db.scala
@@ -21,27 +21,37 @@ abstract class BidAskTicks(tag: Tag, tableName: String, schemaName: Option[Strin
   def * = (time, bid, ask, bid_size, ask_size, bid_decrease, ask_increase, pk)
 }
 
+abstract class TradeTicks(tag: Tag, tableName: String, schemaName: Option[String])
+  extends Table[(Timestamp, Double, Int,/*String ,*/ String)](
+    tag, _tableName=tableName, _schemaName=schemaName
+  ) {
+  def time = column[Timestamp]("time")
+  def price = column[Double]("price")
+  def size = column[Int]("size")
+  def exchange = column[String]("exchange")
+  def pk = column[String]("pk", O.PrimaryKey)
+  def * = (time, price, size,/*exchange,*/ pk)
+}
+
 class BidAskTicksNQ20200918(tag: Tag)
   extends BidAskTicks(tag, "nq20200918_371749745_bid_ask", Some("ticks"))
 
-class OldBidAskTicks(tag: Tag)
-  extends Table[(Timestamp, Double, Double, Int, Int, Boolean, Boolean, String)](
-    tag, _tableName="test", _schemaName=Some("ticks")
+class TradeTicksNQ20200918(tag: Tag)
+  extends TradeTicks(tag, "nq20200918_371749745_trades", Some("ticks"))
+
+class BidAskTicksGrpNQ20200918(tag: Tag)
+  extends Table[(Timestamp, Double, Double)](
+    tag, _tableName="nq20200918_371749745_bid_ask_grp", _schemaName=Some("ticks")
   ) {
   def time = column[Timestamp]("time")
   def bid = column[Double]("bid")
   def ask = column[Double]("ask")
-  def bid_size = column[Int]("bid_size")
-  def ask_size = column[Int]("ask_size")
-  def bid_decrease = column[Boolean]("bid_decrease")
-  def ask_increase = column[Boolean]("ask_increase")
-  def pk = column[String]("pk", O.PrimaryKey)
-  def * = (time, bid, ask, bid_size, ask_size, bid_decrease, ask_increase, pk)
+  def * = (time, bid, ask)
 }
-  
+
 object Db {
   val fileConfig = ConfigFactory.parseFile(new File("application.conf"))
   val config = ConfigFactory.load(fileConfig)
   
-  val Db = Database.forConfig(path="postgres", config=config)
+  val db = Database.forConfig(path="postgres", config=config)
 } 
